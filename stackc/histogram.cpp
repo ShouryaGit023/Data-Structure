25fc87955261b0923e49c10d353617dd9fad081f
Shourya <shouryasinha23@gmail.com>
2025-08-11 23:40:57 +0530

    histogram

---
+++ stackc/histogram.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+//histogram me main is to find right smaller as well as left smaller then just calculate the max area and store the max out of all
+
+int main(){
+    
+}
