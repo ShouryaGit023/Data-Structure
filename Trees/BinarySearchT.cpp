4bbc428a8176d778e108bff7615ef5d7fb278454
Shourya <shouryasinha23@gmail.com>
2025-12-12 12:12:01 +0530

    BST question

---
+++ Trees/BinarySearchT.cpp
@@ -0,0 +1,203 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node{
+    public: 
+    int data;
+    node* left;
+    node* right;
+
+
+    node(int n){
+        this->data=n;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+node* buildTree(node* root){
+    cout<<"Enter the root: "<<endl;
+    int data;cin>>data;
+    root=new node(data);
+    if(data==-1){
+        return NULL;
+    }
+    cout<<"enter the left of "<<data<<endl;
+    root->left=buildTree(root->left);
+    cout<<"enter the right of "<<data<<endl;
+    root->right=buildTree(root->right);
+    return root;
+}
+
+//also the bfs for tree so will use queue
+void level_order(node* root){
+    queue<node*>q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        node* temp=q.front();
+        q.pop();
+        
+        if(temp==NULL){
+            cout<<"/"<<" ";
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }else{
+            cout<<temp->data<<" ";
+            if(temp->left){
+            q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+        }
+
+        
+    }
+}
+
+//inorder LRN Left-Right-Node
+void inorder(node* root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+void preorder(node* root){ // NLR Node-Left-Right
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+
+void postorder(node* root){ // LRN Left-Right-Node
+    if(root==NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+//-----------------------------------------------------------------------------------//
+
+//Q1. Search in BST
+    node* searchBST(node* root, int val) {
+        while(root!=NULL && root->data!=val){
+            if(root->data<val){
+                root=root->right;
+            }
+            else{
+                root=root->left;
+            }
+        }
+        return root;
+    }
+
+//--------------------------------------------
+// Q2. Ceil & Floor in BST
+int findCeil(node* root, int key) {
+        // code here
+        int ceil = -2; 
+
+    while (root) {
+      
+        // If root itself is ceil
+        if (root->data == key) {
+            return root->data; 
+        }
+ 
+        // If root is smaller, the ceil
+        // must be in the right subtree
+        if (key > root->data) {
+            root = root->right; 
+        } 
+      
+        // Else either root can be ceil
+        // or a node in the left child
+        else {
+            ceil = root->data; 
+            root = root->left; 
+        }
+    }
+    return ceil; 
+}
+
+int findFloor(node* root, int x) {
+        // code here
+        int ans=-1;
+        while(root){
+            if(root->data==x){
+                return root->data;
+            }
+
+            if(root->data > x){
+                root=root->left;
+            }
+            else{
+                ans=root->data;
+                root=root->right;
+            }
+        }
+        return ans;
+        
+    }
+
+//-----------------------------------------------------------
+
+//Q6. Validate the BST
+bool check(node* root,int maxi,int mini){
+      if(!root){
+          return true;
+      }
+      if(root->data<=mini || root->data>=maxi){
+          return false;
+      }
+      
+      
+      bool l=check(root->left,root->data,mini);
+      bool r=check(root->right,maxi,root->data);
+      return (l && r);
+  }
+    bool isBST(node* root) {
+        // code here
+        int mini=INT_MIN;
+        int maxi=INT_MAX;
+        return check(root,maxi,mini);
+        
+        
+    }
+
+//---------------------------------------------------------------
+
+//Q7. LCA in BST
+node* LCA(node* root, node* n1, node* n2) {
+        // code here
+        if(n1->data==root->data || n2->data==root->data){
+            return root;
+        }
+        if(n1->data<root->data && n2->data<root->data){
+            LCA(root->left,n1,n2);
+        }
+        else if(n1->data>root->data && n2->data>root->data){
+            LCA(root->right,n1,n2);
+
+        }
+        else{
+            return root;
+        }
+    }
+
+    
+
+
+
+int main(){
+
+}
---
+++ Trees/bst.md
@@ -0,0 +1,41 @@
+## Intro:
+left part should be lesser than the right part
+<br>For example
+            8
+         3       10
+     1     6   9    14
+<br>
+Also Duplicates are not allowed
+
+### WHY?
+the height of BST is log base 2 so the time complexity is not O(n), it will take O(log(n));
+
+
+### Q1. Search in BST
+since this is bst check for the root node val and check to move right or left
+
+### Q2. Ceil in BST
+if the root->val>key ---> go left
+else go right
+<br>Similar for the floor
+
+
+### 3. Insert a given node in BST
+Check where the given value might fit in and then check whether that is null or not and then return that node 
+
+### Q4. Delete a Node of BST
+
+### Q5 kth smallest in BST
+Inorder of BST is always sorted
+just do inorder and keep the count of node 
+for cnt==k print that node thats all
+
+### Q6. Validate a BST
+everything to right of root is larger
+everything to the left should be smaller
+
+### Q7. LCA in BST
+ Use the property of bst
+ if n1 S root && n2 S root --->move to left; <br>
+    n1 B root && n2 B root ---> move to right <br>
+    return node iff any of n1 n2 becomes equal to node or n1 B root S n2
