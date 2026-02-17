4338aeb341bddeeed092e1f348c365b4317ecee9
Shourya <shouryasinha23@gmail.com>
2026-02-17 23:28:12 +0530

    Tabulation

--- Dynamic_Programming/Questions.cpp
+++ Dynamic_Programming/Questions.cpp
@@ -192,6 +192,23 @@ int solve(int m,int n,int a,int b,vector<vector<int>> &dp){
         
         
     }
+
+    //tabulation
+    int numberOfPaths(int m, int n) {
+        // code here
+        vector<vector<int>>dp(m,vector<int>(n,1));
+        
+        // tabulation
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+        
+        
+    }
         
 
 
